b7ac6ec7d4ed89a1e7c52a168878365a186e03d9
Brandon Goldsworthy <BGoldsworthy@neumont.edu>
2021-05-25 09:41:49 -0600

    Added all basic UI & some menu functionality

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System.Collections;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void SetLevelMST(float sliderValue)
+    {
+        mixer.SetFloat("MST", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetLevelBGM(float sliderValue)
+    {
+        mixer.SetFloat("BGM", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetLevelSFX(float sliderValue)
+    {
+        mixer.SetFloat("SFX", Mathf.Log10(sliderValue) * 20);
+    }
+    public void SetLevelPitch(float sliderValue)
+    {
+        mixer.SetFloat("Pitch", sliderValue);
+    }
+
+
+    public void Mute(bool mute)
+    {
+        if (mute) mixer.SetFloat("MST", -80);
+        else mixer.SetFloat("MST", 0);
+    }
+}
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    [Header("Panels")]
+    public GameObject MainMenuPanel;
+    public GameObject OptionsPanel;
+    public GameObject CreditsPanel;
+    public GameObject PausePanel;
+    public GameObject InstructionsPanel;
+    public GameObject GamePanel;
+    public GameObject ScenePanel;
+
+    [Header("Sub-Panels")]
+    //Maybe?
+
+    [Header("Other")]
+    public GameController gameController;
+    List<GameObject> gameObjects = new List<GameObject>();
+
+    private void Start()
+    {
+        gameObject.SetActive(true);
+        gameObjects.Add(MainMenuPanel);
+        gameObjects.Add(OptionsPanel);
+        gameObjects.Add(CreditsPanel);
+        gameObjects.Add(PausePanel);
+        gameObjects.Add(InstructionsPanel);
+
+        Disable();
+        MainMenuPanel.SetActive(true);
+        GameController.Instance.state = eState.TITLE;
+    }
+
+    public void Disable()
+    {
+        foreach (GameObject gameObject in gameObjects)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    public void StartGame()
+    {
+        Disable();
+        GamePanel.SetActive(true);
+        GameController.Instance.state = eState.GAME;
+        Debug.Log("Start Game");
+    }
+
+    public void Options()
+    {
+        Disable();
+        OptionsPanel.SetActive(true);
+        Debug.Log("Options menu");
+    }
+
+    public void Instructions()
+    {
+        Disable();
+        InstructionsPanel.SetActive(true);
+        GameController.Instance.state = eState.INSTRUCTIONS;
+    }
+
+    public void Credits()
+    {
+        Disable();
+        CreditsPanel.SetActive(true);
+        Debug.Log("Credits menu");
+    }
+
+    public void Back()
+    {
+        Disable();
+
+        if (GameController.Instance.state == eState.PAUSE)
+        {
+            BackToPause();
+        }
+        else
+        {
+            BackToMenu();
+        }
+    }
+
+    public void Pause()
+    {
+        if (GameController.Instance.state == eState.GAME)
+        {
+            Disable();
+            PausePanel.SetActive(true);
+            GameController.Instance.state = eState.PAUSE;
+        }
+    }
+
+    //Back to main menu
+    public void BackToMenu()
+    {
+        Disable();
+        MainMenuPanel.SetActive(true);
+        GameController.Instance.state = eState.TITLE;
+    }
+
+    //Back to pause menu
+    public void BackToPause()
+    {
+        Disable();
+        PausePanel.SetActive(true);
+        GameController.Instance.state = eState.PAUSE;
+    }
+
+    public void ResetApplication()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
